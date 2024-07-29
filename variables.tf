ea0aeb5673bad82ebbf223bf05e59725c63d247f
armdupre <a-joseph.dupre@keysight.com>
2024-07-29 07:48:00 -0400

    Updated.

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "AmiName" {
-	default = "Ixia_Virtual_Test_Appliance_10.00.0.140_KVM BYOL-eaa16046-9580-4e91-8167-302d8cb0da84"
+	default = "Ixia_Virtual_Test_Appliance_10.40.0.75-eaa16046-9580-4e91-8167-302d8cb0da84"
 	description = "AMI name used for deploying instances"
 	type = string
 }
@@ -134,7 +134,7 @@ variable "UserProjectTag" {
 }
 
 variable "Version" {
-	default = "10-00"
+	default = "10-40"
 	description = "Versioning of the application using the deployment"
 	type = string
 }
